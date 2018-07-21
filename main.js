9a7ea4fff0c167bba35be98ca607adac32bec902
Tucker <trblackwell1221@email.campbell.edu>
2018-07-21 00:24:44 -0400

    Revamped the UI and changed what each posts adds to the UI

--- main.js
+++ main.js
@@ -1,22 +1,16 @@
 //html elements
-const ul = document.querySelector('ul');
-const select = document.querySelector('select');
-const container = document.querySelector('.container');
-const catButton = document.querySelector('#poker-cat');
-
-//not in use yet
-const score100 = document.querySelector('#hundred');
-const score50 = document.querySelector('#fifty');
-const score25 = document.querySelector('#twentyFive');
-
-
+const ul = document.querySelector("ul");
+const select = document.querySelector("select");
+const container = document.querySelector(".container");
+const catButton = document.querySelector("#poker-cat");
+const postText = document.querySelector(".postText");
+const readMoreButton = document.querySelector("#readMore");
 
 //brings the cat image back
-catButton.addEventListener('click', () => {
+catButton.addEventListener("click", () => {
   ul.innerHTML = `<li class="text-center"><img src="http://www.guzer.com/pictures/poker-cat.jpg" alt="poker-cat" class="text-center m-auto col-sm-12 col-md-12 col-lg-10 col-xl-8"></li>
-          <li class="text-center">"Pick a sub, any sub, but make it quick!"</li>`
+          <li class="text-center">"Pick a sub, any sub, but make it quick!"</li>`;
 });
-
 
 //get top 5 posts (no filter)
 const fetchTopFive = async sub => {
@@ -25,26 +19,30 @@ const fetchTopFive = async sub => {
   const response = await fetchResult;
   const jsonData = await response.json();
   const posts = jsonData.data.children;
-  const content = posts.map((post, i) => (`<li class='list-group-item'><strong>Post ${i + 1}: </strong><em>"${post.data.title}"</em> (/u/${post.data.author}) <a href='${post.data.url}' class='badge badge-info' target="_blank">link to post</a></li>`)).join('');
+  const content = posts
+    .map(
+      (post, i) =>
+        `<li class='list-group-item'>
+        <strong>${i + 1}) "${post.data.title}"</strong>
+         <small>(/u/${post.data.author})</small> 
+         <a href='https://reddit.com${
+           post.data.permalink
+         }' class='badge' target="_blank">link to post</a>
+         <button disabled class="btn">Post score: <strong>${post.data.score}</strong></button>
+         <p class="postText lead" style="display: none">${
+           post.data.selftext
+         }</p>
+      </li>`
+    )
+    .join("");
   ul.innerHTML = content;
-  console.log(posts)
-
-
-}
-
+  console.log(posts);
+};
 
 // passes selected sub to fetchTopFive()
 function selectSub() {
   fetchTopFive(this.value);
 }
 
-
-
-select.addEventListener('change', selectSub);
-
-
- //filter listeners
- //trying to find a way to apply filters to json after it's already on the page--difficult
-
-
- //fetchTopFive('javascript');
+select.addEventListener("change", selectSub);
+readMoreButton.addEventListener('click', () => postText.style.display === 'inherit');
