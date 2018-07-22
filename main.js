f0bce64571fda59e7848c5ddb8e950134349c100
Tucker <trblackwell1221@email.campbell.edu>
2018-07-22 00:50:44 -0400

    Added a comment count to each li; still working on trying to make 'read more' button

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ const ul = document.querySelector("ul");
 const select = document.querySelector("select");
 const container = document.querySelector(".container");
 const catButton = document.querySelector("#poker-cat");
-const postText = document.querySelector(".postText");
+const selfText = document.querySelector("#selfText");
 const readMoreButton = document.querySelector("#readMore");
+//const postRangeSelector = document.querySelector("#postRangeSelector");
 
 //brings the cat image back
 catButton.addEventListener("click", () => {
   ul.innerHTML = `<li class="text-center"><img src="http://www.guzer.com/pictures/poker-cat.jpg" alt="poker-cat" class="text-center m-auto col-sm-12 col-md-12 col-lg-10 col-xl-8"></li>
           <li class="text-center">"Pick a sub, any sub, but make it quick!"</li>`;
 });
+
 
 //get top 5 posts (no filter)
 const fetchTopFive = async sub => {
@@ -23,16 +25,25 @@ const fetchTopFive = async sub => {
     .map(
       (post, i) =>
         `<li class='list-group-item'>
-        <strong>${i + 1}) "${post.data.title}"</strong>
+        <strong>${i + 1})</strong>"<strong><a id="postTitleLink" href='https://reddit.com${
+          post.data.permalink
+        }'>${post.data.title}</strong></a>"
          <small>(/u/${post.data.author})</small> 
          <a href='https://reddit.com${
            post.data.permalink
          }' class='badge' target="_blank">link to post</a>
-         <button disabled class="btn">Post score: <strong>${post.data.score}</strong></button>
-         <button class="btn">Read more </button>
-         <p class="postText lead" style="display: none">${
-           post.data.selftext
-         }</p>
+         <button disabled class="btn">Post score: <strong>${
+           post.data.score
+         }</strong></button>
+         <button disabled class="btn">Comments: <strong>${
+          post.data.num_comments
+        }</strong></button>
+         <button class="btn btn-sm" id="readMore">Read more</button>
+         <div id="selfText" style="display: none;">
+         <hr>
+         <p class="postText lead">${post.data.selftext}</p>
+         </div>
+         
       </li>`
     )
     .join("");
@@ -46,4 +57,4 @@ function selectSub() {
 }
 
 select.addEventListener("change", selectSub);
-readMoreButton.addEventListener('click', () => postText.style.display === 'inherit');
+//readMoreButton.addEventListener('click', () => postText.style.display === 'inherit');
