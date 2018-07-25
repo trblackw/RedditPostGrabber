549507471fb6e568ce7e4a2498ed972409cbec6f
Tucker <trblackwell1221@email.campbell.edu>
2018-07-24 20:08:27 -0400

    Added new regex functionality to test/format links, included more select values (subreddits), revamped UI a bit (added new fonts); included pictures for each post, if available; still much to do

--- main.js
+++ main.js
@@ -7,88 +7,101 @@ const selfText = document.querySelector("#selfText");
 const readMoreButton = document.querySelector("#readMore");
 //const postRangeSelector = document.querySelector("#postRangeSelector");
 
-//brings the cat image back
+//brings the cat image back; admittedly stupid
 catButton.addEventListener("click", () => {
   ul.innerHTML = `<li class="text-center"><img src="http://www.guzer.com/pictures/poker-cat.jpg" alt="poker-cat" class="text-center m-auto col-sm-12 col-md-12 col-lg-10 col-xl-8"></li>
-          <li class="text-center">"Pick a sub, any sub, but make it quick!"</li>`;
+          <li class="text-center text-light">"Pick a sub, any sub, but make it quick!"</li>`;
 });
 
+//handles either reddit's format of links (linktitle)[url] or inline links 'https//...'
 const linkFormatter = linkStr => {
-  const regex = /(\[.+\])(\(.+\))/gi;
-  return linkStr
-    .replace(regex, `<a href='\$2' id="selfTextLink">\$1</a>`)
-    .replace(/\[|\]/g, "");
+  const redditLink = /(\[.+\])(\(.+\))/gi;
+  const inlineLink = /(https|http)(\W*)(\w+)(\.com.*?)/gi;
+  linkStr = linkStr
+   .replace(redditLink, `<a href='\$2' class="selfTextLink">\$1</a>`)
+   .replace(/\[|\]|\(|\)/g, "");
+  return linkStr.replace(inlineLink, `<a href='\$1\$2\$3\$4' class="selfTextLink">\$3\$4</a>`)
 };
 
+//forms (mm/dd time of post) format
 const dateFormatter = timeStamp => {
   const date = new Date(timeStamp * 1000).toLocaleDateString().split("/");
   const time = new Date(timeStamp * 1000).toLocaleTimeString().split(":");
   return `${date[0]}/${date[1]} ${time[0]}:${time[1]}pm`;
 };
 
-console.log(dateFormatter(1532307020));
-//get top 5 posts (no filter)
 const fetchTopFive = async sub => {
   const URL = `https://www.reddit.com/r/${sub}/top/.json?limit=5`;
   const fetchResult = fetch(URL);
   const response = await fetchResult;
   const jsonData = await response.json();
   const posts = jsonData.data.children;
-  const content = posts
-    .map(
-      (post, i) =>
-        `<li class='list-group-item'>
-        <strong><a id="postTitleLink" href='https://reddit.com${
-          post.data.permalink
-        }' target='_blank'>${i + 1}) "${
-          post.data.title
-        }"</strong></a><a href='https://reddit.com${
-          post.data.permalink
-        }'><i class='fas fa-link'></i></a>
-        <small class='text-right'>(/u/${post.data.author})</small>
-        <div class='row'>
-         
-         <button disabled class="btn col">Post score: <strong>${
-           post.data.score
-         }</strong></button>
-         <button disabled class="btn col">Comments: <strong>${
-           post.data.num_comments
-         }</strong></button>
+  ul.innerHTML = posts
+    .map((post, i) => {
+      const {
+        permalink,
+        title,
+        author,
+        score,
+        num_comments,
+        created_utc,
+        selftext,
+        preview
+      } = post.data;
+      //if there is no image to display, display just the text
+      if (!preview) {
+        return `
+        <li class='list-group-item'>
+        <strong><a id="postTitleLink" href='https://reddit.com${permalink}' target='_blank'>${i +
+          1}) "${title}"</strong></a><a href='https://reddit.com${permalink}'><i class='fas fa-link'></i></a>
+        <small class='text-right'>(/u/${author})</small>
+        <div class='container justify-content-end row'>
+         <button disabled class="btn mx-3 p-2"><strong><i class="fas fa-arrow-up"></i> ${score}</strong></button>
+         <button disabled class="btn mx-3 p-2"><strong><i class="fas fa-comments"></i> ${num_comments}</strong></button>
         </div>
-        <small class="d-block text-muted">${dateFormatter(
-          post.data.created_utc
-        )}
+        <small class="d-block text-muted">${dateFormatter(created_utc)}
         </small>
          <div id="selfText" style="display: inherit; overflow: scroll;">
          <hr>
          <p class="postText lead">${
-           post.data.selftext
-             ? linkFormatter(post.data.selftext)
-             : `<small class="text-muted">No post text to display</small>`
-         }</p>
-         
-         </div>
-
-         
-      </li>`
-    )
+           selftext
+             ? linkFormatter(selftext)
+             : `<p class='text-muted small d-inline'>There's no text or media to display for this post</p>`
+         }</p>   
+       
+         </div> 
+      </li>
+      `; //if there is an image, display the image
+      } else {
+        const { url } = preview.images[0].source;
+        console.log(preview.images);
+        return `
+      <li class='list-group-item'>
+        <strong><a id="postTitleLink" href='https://reddit.com${permalink}' target='_blank'>${i +
+          1}) "${title}"</strong></a><a href='https://reddit.com${permalink}'><i class='fas fa-link'></i></a>
+        <small class='text-right'>(/u/${author})</small>
+        <div class='container justify-content-end row'>
+         <button disabled class="btn mx-3 p-2"><i class="fas fa-arrow-up"></i> ${score}</strong></button>
+         <button disabled class="btn mx-3 p-2"><strong><i class="fas fa-comments"></i> ${num_comments}</strong></button>
+        </div>
+        <small class="d-block text-muted">${dateFormatter(created_utc)}
+        </small>
+         <div id="selfText" style="display: inherit; overflow: scroll;">
+         <hr>
+         <p class="postText lead">${
+          selftext ? linkFormatter(selftext) : ``
+          }</p>   
+         <div class='container m-auto justify-content-center'>
+            <a href='${url}'><img src=${url} width=100% height=auto class='img-fluid' id='postImg'></a>
+        </div>
+        </div> 
+      </li>
+      `;
+      }
+    })
     .join("");
-  // ul.innerHTML = content;
-  // console.log(posts);
-
-  // const vidContainer = document.createElement('div');
-  // vidContainer.innerHTML = `
-  // <video width='${posts[0].data.media.reddit_video.width}' height='${
-  //     posts[0].data.media.reddit_video.height
-  //     }'
-  //     <source src='${posts[0].data.media.reddit_video.dash_url}'
-  //   </video>
-  //   `
-  //   ul.appendChild(vidContainer)
-  // getting somewhere with this
+  console.log(posts);
 };
-
-
 
 // passes selected sub to fetchTopFive()
 function selectSub() {
